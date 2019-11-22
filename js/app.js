9176201512a45d0fefada11574a76358545b4b84
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2019-11-22 14:49:10 -0800

    added username feature to index page.  added getname function to app.js to collect name from index

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 'use strict';
-// PLACEHOLDER STARTING ATTRIBUTES - GLOBAL VARIABLES
-var player = new Player(prompt('What is your name?'))
+// USERNAME
+//
+// function gets name from index.html text field for user name
+function getName() {
+    var userNameInputElement = document.getElementById('nameOfPlayer');
+    var userName = userNameInputElement.value;
+    var nameJSON = JSON.stringify(userName);
+    localStorage.setItem('userName', nameJSON);
+  }
+// Get user name from local storage and set player name
+var localStorageUserName = localStorage.getItem('userName');
+var player = JSON.parse(localStorageUserName);
+
 // Starting player attributes should be determined by level, default levels set in player constructor function.
 var startingMoney;
 var startingTime;
